23c291fc35548ceb538aabd316eda204a7ed8e6c
Geografia das Coisas <86194340+geografiadascoisas@users.noreply.github.com>
2025-12-08 10:11:15 -0300

    Refactor: corrected dialog initialization (super()), removed redundant QDialog.init(), and standardized PyQt5 imports for QGIS compatibility

--- hotspot_analysis_dialog.py
+++ hotspot_analysis_dialog.py
@@ -1,42 +1,45 @@
 # -*- coding: utf-8 -*-
 """
 /***************************************************************************
- HotspotAnalysisDialog
-                                 A QGIS plugin
- This plugin generates data needed for hotspot Analysis
-                             -------------------
-        begin                : 2016-06-19
+ Hotspot Analysis v3.0.1 (enhanced 2025)
+                                 A QGIS Plugin
+
+ This plugin implements Local Indicators of Spatial Association (LISA),
+ including Getis-Ord Gi* and Moran-based cluster analysis.
+
+ Enhanced version maintained for compatibility with modern QGIS and
+ libpysal/esda libraries, with corrections to statistical output fields
+ and improved robustness for educational and analytical workflows.
+
+ -------------------
+        begin                : 2017-02-22
+        original authors     : Daniele Oxoli, Gabriele Prestifilippo,
+                               Mayra Zurbaràn, Stanly Shaji
+        email (original)     : daniele.oxoli@polimi.it
+        maintenance (2025)   : Abimael Cereda Junior
+        email (2025)         : ceredajunior@geografiadascoisas.com.br
+        version              : 3.0.1
+        date                 : 2025-12-07
         git sha              : $Format:%H$
-        copyright            : (C) 2016 by Stanly Shaji, Arunkumar / Politecnico Di Milano
-        email                : stanly.shaji@mail.polimi.it
  ***************************************************************************/
 
 /***************************************************************************
- *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
- *                                                                         *
  ***************************************************************************/
 """
 
 import os
-from qgis.PyQt import QtGui, uic
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QDialog
+from PyQt5 import QtWidgets, uic
 
-FORM_CLASS, _ = uic.loadUiType(os.path.join(
-    os.path.dirname(__file__), 'hotspot_analysis_dialog_base.ui'))
+FORM_CLASS, _ = uic.loadUiType(
+    os.path.join(os.path.dirname(__file__), 'hotspot_analysis_dialog_base.ui')
+)
 
-class HotspotAnalysisDialog(QDialog, FORM_CLASS):
+class HotspotAnalysisDialog(QtWidgets.QDialog, FORM_CLASS):
     def __init__(self, parent=None):
         """Constructor."""
-        super(HotspotAnalysisDialog, self).__init__(parent)
-        # Set up the user interface from Designer.
-        # After setupUI you can access any designer object by doing
-        # self.<objectname>, and you can use autoconnect slots - see
-        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
-        # #widgets-and-dialogs-with-auto-connect
-        QDialog.__init__(self)
+        super().__init__(parent)
         self.setupUi(self)
